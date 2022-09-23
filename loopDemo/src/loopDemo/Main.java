af5fc77030b964954106e50a8314b809dd972852
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-09-23 23:31:34 +0300

    update

--- loopDemo/src/loopDemo/Main.java
+++ loopDemo/src/loopDemo/Main.java
@@ -2,12 +2,31 @@ package loopDemo;
 
 public class Main {
 	public static void main(String args[]) {
-
+		
+		// for
+		
 		for (int i = 0; i < 10; i += 2) {
 			System.out.println(i);
-
 		}
 		System.out.println("Dongu bitti");
-	}
 
+		// while
+		
+		int j = 0;
+		while (j < 10) {
+			j += 2;
+			System.out.println(j);
+		}
+		System.out.println("Dongu bitti");
+		
+		// do while
+		
+		int n = 0;
+		do {
+			n += 2;
+			System.out.println(n);
+		} while (n < 10);
+		System.out.println("Dongu bitti");
+
+	}
 }
