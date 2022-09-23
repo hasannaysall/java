91352071281b866ef352d3aaa6848e93d06c6709
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-09-23 17:43:04 +0300

    deneme

---
+++ oop/src/oop/Customer.java
@@ -0,0 +1,31 @@
+package oop;
+
+public class Customer {
+	private int id;
+	private String customerNumber;
+	private String number;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCustomerNumber() {
+		
+		return customerNumber;
+	}
+	public void setCustomerNumber(String customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	
+	
+
+}
---
+++ oop/src/oop/Main.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Customer customer = new Customer();
+	customer.setId(45632);
+	customer.setCustomerNumber("654782");
+	customer.setNumber("75");
+	System.out.println(customer.getId());
+	
+	
+	
+			
+}
+}
