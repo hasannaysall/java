1ca6b1911ed0b3cfa956f6f806cce2105317d684
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-09-23 22:14:37 +0300

    update

--- recapDemo1/src/recapDemo1/Main.java
+++ recapDemo1/src/recapDemo1/Main.java
@@ -3,20 +3,18 @@ package recapDemo1;
 public class Main {
 	public static void main(String args[]) {
 		int sayi1 = 15;
-		int sayi2=78;
-		int sayi3=-14;
-		int enBuyuk=sayi1;
-		
-		if(enBuyuk<sayi2) {
-			enBuyuk=sayi2;
+		int sayi2 = 78;
+		int sayi3 = -14;
+		int enBuyuk = sayi1;
+
+		if (enBuyuk < sayi2) {
+			enBuyuk = sayi2;
 		}
-	
-		else if(enBuyuk<sayi3) {
-				enBuyuk=sayi3;
-			}
+
+		else if (enBuyuk < sayi3) {
+			enBuyuk = sayi3;
+		}
 		System.out.println(enBuyuk);
-		}
-		
 	}
 
-
+}
