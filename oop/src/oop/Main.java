1fcb2b79f93ae1754d684b0aa2853cfb0ad184af
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-09-23 18:01:20 +0300

    deneme

--- oop/src/oop/Main.java
+++ oop/src/oop/Main.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 	Customer customer = new Customer();
 	customer.setId(45632);
 	customer.setCustomerNumber("654782");
-	customer.setNumber("75");
+	customer.setNumber("75444");
 	System.out.println(customer.getId());
 	
 	
