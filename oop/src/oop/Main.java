f9aaa8bdb223137f07cb43a43ed3e5b54aec4039
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-09-23 18:38:02 +0300

    update

--- oop/src/oop/Main.java
+++ oop/src/oop/Main.java
@@ -3,11 +3,39 @@ package oop;
 public class Main {
 public static void main(String[] args) {
 	
-	Customer customer = new Customer();
-	customer.setId(45632);
-	customer.setCustomerNumber("654782");
-	customer.setNumber("75");
-	System.out.println(customer.getId());
+	Customer customer1 = new Customer();
+	customer1.setId(45632);
+	customer1.setCustomerNumber("654782");
+	customer1.setNumber("75");
+	
+	Product product1 = new Product();
+	product1.setName("b marka kahve makinesi");
+	product1.setUnitPrice(2500);
+	product1.setDiscount(6);
+	product1.setUnitInStock(14);
+	product1.setImageUrl("adres2.jpg");
+
+	Product product2 = new Product();
+	product2.setName("c marka kahve makinesi");
+	product2.setUnitPrice(750);
+	product2.setDiscount(9);
+	product2.setUnitInStock(25);
+	product2.setImageUrl("adres3.jpg");
+	
+	Product product3 = new Product();
+	product3.setName("d marka kahve makinesi");
+	product3.setUnitPrice(1400);
+	product3.setDiscount(6);
+	product3.setUnitInStock(5);
+	product3.setImageUrl("adres4.jpg");
+	
+	Product[] products = {product1,product2,product3};
+	for (Product product : products) {
+		System.out.println(product.getName());
+	}
+	
+	
+	
 	
 	
 	
---
+++ oop/src/oop/Product.java
@@ -0,0 +1,42 @@
+package oop;
+
+public class Product {
+	private String name;
+	private int unitPrice;
+	private int discount;
+	private int unitInStock;
+	private String imageUrl;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public int getDiscount() {
+		return discount;
+	}
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+	public int getUnitInStock() {
+		return unitInStock;
+	}
+	public void setUnitInStock(int unitInStock) {
+		this.unitInStock = unitInStock;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+	
+
+}
