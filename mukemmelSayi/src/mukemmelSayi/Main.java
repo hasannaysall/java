323de9970bd4404a60b25e56ad5f2396e1c8df95
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-10-06 15:26:43 +0300

    update

---
+++ mukemmelSayi/src/mukemmelSayi/Main.java
@@ -0,0 +1,18 @@
+package mukemmelSayi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 6;
+		int toplam = 0;
+		for (int i = 1; i < 6; i++) {
+			if (sayi % i == 0)
+				toplam += i;
+		}
+		if (sayi == toplam)
+			System.out.println("Sayi mukemmel");
+		else
+			System.out.println("Sayi mukemmel degil");
+	}
+
+}
