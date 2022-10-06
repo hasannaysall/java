3717156099949731c7ebe7cb666dbed32db2540a
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-10-06 14:25:32 +0300

    update

--- stringsDemo/src/stringsDemo/Main.java
+++ stringsDemo/src/stringsDemo/Main.java
@@ -15,6 +15,18 @@ public class Main {
 		System.out.println(karakterler);
 		System.out.println(mesaj.indexOf("a"));// a nın bastan kacıncı oldugunu gosteriri
 		System.out.println(mesaj.lastIndexOf("a"));
+		
+		String yeniMesaj = mesaj.replace(' ', '-');//bosluklara - ekledi
+		System.out.println(yeniMesaj);
+		System.out.println(mesaj.substring(2, 4));//2.indexten baslayıp 3.indexse kadar dondurur
+		
+		for (String kelime : mesaj.split(" ")) {//bosluklari atarak yazdiracak
+			System.out.println(kelime);
+		}
+		
+		System.out.println(mesaj.toLowerCase());//tum harfleri kucuk yapar
+		System.out.println(mesaj.toUpperCase());//tum harfleri buyuk yapar
+		System.out.println(mesaj.trim());//metnin basindaki ve sonundaki bosluklari alır
 	}
 
 }
