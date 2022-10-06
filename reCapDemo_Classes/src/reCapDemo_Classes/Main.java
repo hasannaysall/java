a52773f908c52073923b016239948c46d8c6761c
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-10-06 23:05:29 +0300

    update

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,15 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+
+		System.out.println(dortIslem.topla(12, 3));
+		System.out.println(dortIslem.cıkar(25, 9));
+		System.out.println(dortIslem.carp(12, 8));
+		System.out.println(dortIslem.bol(12, 4));
+
+	}
+
+}
