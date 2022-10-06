3cf94cdd38d6e70595e39f4dd45e5c8e9341c73f
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-10-06 21:32:10 +0300

    update

---
+++ methods/src/methods/Main.java
@@ -0,0 +1,28 @@
+package methods;
+
+public class Main {
+	public static void sayiBul() {
+		int sayi1 = 220;
+		int sayi2 = 284;
+		int toplam1 = 0;
+		int toplam2 = 0;
+		for (int i = 1; i < sayi1; i++) {
+			if (sayi1 % i == 0)
+				toplam1 += i;
+		}
+		for (int j = 1; j < sayi2; j++) {
+			if (sayi2 % j == 0)
+				toplam2 += j;
+		}
+		if (toplam1 == sayi2 && toplam2 == sayi1)
+			System.out.println("Arkadas sayilar");
+		else
+			System.out.println("Arkadas sayi degiller");
+	}
+
+	public static void main(String[] args) {
+		sayiBul();
+
+	}
+
+}
