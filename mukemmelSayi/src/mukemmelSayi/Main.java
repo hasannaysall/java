2c4657699631da64994e7e46c811a61029cb897e
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-10-06 15:28:46 +0300

    update

--- mukemmelSayi/src/mukemmelSayi/Main.java
+++ mukemmelSayi/src/mukemmelSayi/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		int sayi = 6;
 		int toplam = 0;
-		for (int i = 1; i < 6; i++) {
+		for (int i = 1; i < sayi; i++) {
 			if (sayi % i == 0)
 				toplam += i;
 		}
