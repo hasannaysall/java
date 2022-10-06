548db6fc7e31793c74790e8d15daff612b090daf
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-10-06 22:00:00 +0300

    update

--- methods2/src/methods2/Main.java
+++ methods2/src/methods2/Main.java
@@ -5,10 +5,17 @@ public class Main {
 		return sayi1 + sayi2;
 
 	}
+	public static int topla2(int... sayilar) {
+		int toplam=0;
+		for(int i=0;i<sayilar.length;i++) {
+			toplam+=sayilar[i];
+		}
+		return toplam;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(topla(12, 3));
-
+		System.out.println(topla2(12,78,14,35));
 	}
 
 }
