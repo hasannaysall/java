3f95f158ea41d0286f7f150f369fc662a0ec9aa1
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-10-06 21:48:55 +0300

    update

---
+++ methods2/src/methods2/Main.java
@@ -0,0 +1,14 @@
+package methods2;
+
+public class Main {
+	public static int topla(int sayi1, int sayi2) {
+		return sayi1 + sayi2;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(topla(12, 3));
+
+	}
+
+}
