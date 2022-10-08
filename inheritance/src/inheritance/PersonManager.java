3c692eab599e708ffa447858699cee7278b2b3d1
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-10-08 13:22:18 +0300

    update

---
+++ inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class PersonManager {
+	public void List() {
+		System.out.println("Musteri listelendi");
+	}
+	public void Add() {
+		System.out.println("Musteri eklendi");
+	}
+}
