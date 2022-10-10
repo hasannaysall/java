bac2ce28f5546abbe8ab1ea50276df48a379514e
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-10-10 20:57:53 +0300

    update

---
+++ interfaces/src/interfaces/OracleCustomerManager.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public class OracleCustomerManager implements ICustomerDal,IRepository{
+
+	@Override
+	public void add() {
+		System.out.println("Oracle eklendi");
+		
+	}
+
+}
